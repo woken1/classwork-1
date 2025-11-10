4b64eb33c701678dc8f69b6647c13c165f42962e
root <root@Asus.localdomain>
2025-11-10 12:47:10 +0300

    commit 4

--- main.cpp
+++ main.cpp
@@ -12,15 +12,23 @@ int main()
   int next = 0;
   std::cin >> next;
 
-  if(!std::cin)
-    return 1;
-  
   IntArray a;
-  
+
   a.add(next);
+
+  while (std::cin >> next)
+  {
+    a.add(next);
+  }
+  
+
+  if(!std::cin)
+  {
+    return 1;
+  };
 
   for (size_t i = 0; i < a.size(); ++i)
   {
-
+    int d = a.get(i);
   }
 };
