c3e8c28ea10a3e6c072539c7c2d80ad8af66df7d
root <root@Asus.localdomain>
2025-11-10 13:19:38 +0300

    commit 7

--- main.cpp
+++ main.cpp
@@ -1,61 +1,70 @@
 #include <iostream>
+#include <stdexcept>
 
 struct IntArray
 {
- IntArray(int i) :
-  k(1),
-  a(new int[1])
- {
-  
+    IntArray(int i) :
+        k(1),
+        a(new int[1])
+    {
+        *a = i;
+    }
 
- }
+    ~IntArray()
+    {
+        delete[] a;
+    }
 
- ~IntArray()
- {
-  delete[] a;
- }
+    int add(int i);
+    int get(size_t id) const;
+    size_t size() const;
+    int last() const;
 
- int add(int i);
- int get(size_t id) const;
- size_t size() const;
- int last() const;
-
- int* a;
- size_t k;
+    int* a;
+    size_t k;
 };
 
 int main()
 {
- try
- {
-  int next = 0;
-  std::cin >> next;
+    try
+    {
+        int next = 0;
+        std::cin >> next;
 
-  IntArray a(next);
+        IntArray a(next);
 
-  while (std::cin >> next)
-  {
-   a.add(next);
-  }
+        while (std::cin >> next)
+        {
+            a.add(next);
+        }
 
-  if (!std::cin && !std::cin.eof())
-  {
-   return 1;
-  }
+        if (!std::cin && !std::cin.eof())
+        {
+            return 1;
+        }
 
-  size_t count = 1;
+        size_t count = 1;
 
-  for (size_t i = 0; i < a.size() - 1; ++i)
-  {
-   int d = a.get(i);
+        for (size_t i = 0; i < a.size() - 1; ++i)
+        {
+            int d = a.get(i);
+            count += !(d % a.last()) ? 1 : 0;
+        }
 
-   count += !(d % a.last()) ? 1 : 0;
-  }
+        std::cout << count << "\n";
+    }
+    catch (...)
+    {
+        return 1;
+    }
+}
 
-  std::cout << count << "\n";
- }
- catch (...)
- {
-  return 1;
- }
+int IntArray::get(size_t id) const
+{
+    if (id >= k)
+    {
+        throw std::logic_error("ERROR: bad id");
+    }
+
+    return a[id];
 }
