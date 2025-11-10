50bdde2d1d3876dc67f652428792e630b89e3fee
root <root@Asus.localdomain>
2025-11-10 13:01:38 +0300

    commit 6

--- main.cpp
+++ main.cpp
@@ -3,35 +3,42 @@
 struct IntArray {
   IntArray(int i) {}
 
-  ~IntArray() {}
+  ~IntArray() { delete[] a; }
 
   int add(int i);
   int get(size_t id) const;
   size_t size() const;
   int last() const;
+
+  int* a;
+  size_t k;
 };
 
 int main() {
-  int next = 0;
-  std::cin >> next;
+  try {
+    int next = 0;
+    std::cin >> next;
 
-  IntArray a(next);
+    IntArray a(next);
 
-  while (std::cin >> next) {
-    a.add(next);
-  }
+    while (std::cin >> next) {
+      a.add(next);
+    }
 
-  if (!std::cin) {
+    if (!std::cin && !std::cin.eof()) {
+      return 1;
+    }
+
+    size_t count = 1;
+
+    for (size_t i = 0; i < a.size() - 1; ++i) {
+      int d = a.get(i);
+
+      count += !(d % a.last()) ? 1 : 0;
+    }
+
+    std::cout << count << "\n";
+  } catch (...) {
     return 1;
   }
-
-  size_t count = 1;
-
-  for (size_t i = 0; i < a.size() - 1; ++i) {
-    int d = a.get(i);
-
-    count += !(d % a.last()) ? 1 : 0;
-  };
-
-  std::cout << count << "\n";
 }
