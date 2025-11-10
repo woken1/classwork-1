62876f8b43e2155c19f085c3b2d79afc3956d8f8
root <root@Asus.localdomain>
2025-11-10 12:43:36 +0300

    commit 3

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 
+struct IntArray
+{
+  int add(int i);
+  int get(size_t id) const;
+  size_t size() const;
+};
+
 int main()
 {
   int next = 0;
   std::cin >> next;
-  if(!std::cin){
+
+  if(!std::cin)
     return 1;
+  
+  IntArray a;
+  
+  a.add(next);
+
+  for (size_t i = 0; i < a.size(); ++i)
+  {
+
   }
 };
