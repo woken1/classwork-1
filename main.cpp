db297de63202a5db9459ad3a822a368314af460a
root <root@Asus.localdomain>
2025-11-10 13:03:40 +0300

    commit 7

--- main.cpp
+++ main.cpp
@@ -1,44 +1,61 @@
 #include <iostream>
 
-struct IntArray {
-  IntArray(int i) {}
+struct IntArray
+{
+ IntArray(int i) :
+  k(1),
+  a(new int[1])
+ {
+  
 
-  ~IntArray() { delete[] a; }
+ }
 
-  int add(int i);
-  int get(size_t id) const;
-  size_t size() const;
-  int last() const;
+ ~IntArray()
+ {
+  delete[] a;
+ }
 
-  int* a;
-  size_t k;
+ int add(int i);
+ int get(size_t id) const;
+ size_t size() const;
+ int last() const;
+
+ int* a;
+ size_t k;
 };
 
-int main() {
-  try {
-    int next = 0;
-    std::cin >> next;
+int main()
+{
+ try
+ {
+  int next = 0;
+  std::cin >> next;
 
-    IntArray a(next);
+  IntArray a(next);
 
-    while (std::cin >> next) {
-      a.add(next);
-    }
-
-    if (!std::cin && !std::cin.eof()) {
-      return 1;
-    }
-
-    size_t count = 1;
-
-    for (size_t i = 0; i < a.size() - 1; ++i) {
-      int d = a.get(i);
-
-      count += !(d % a.last()) ? 1 : 0;
-    }
-
-    std::cout << count << "\n";
-  } catch (...) {
-    return 1;
+  while (std::cin >> next)
+  {
+   a.add(next);
   }
+
+  if (!std::cin && !std::cin.eof())
+  {
+   return 1;
+  }
+
+  size_t count = 1;
+
+  for (size_t i = 0; i < a.size() - 1; ++i)
+  {
+   int d = a.get(i);
+
+   count += !(d % a.last()) ? 1 : 0;
+  }
+
+  std::cout << count << "\n";
+ }
+ catch (...)
+ {
+  return 1;
+ }
 }
