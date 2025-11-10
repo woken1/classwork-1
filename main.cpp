c351c5c3962372ad72b6ee8e9a90dc169d8814e3
root <root@Asus.localdomain>
2025-11-10 12:58:05 +0300

    commit 5

--- main.cpp
+++ main.cpp
@@ -1,34 +1,37 @@
 #include <iostream>
 
-struct IntArray
-{
+struct IntArray {
+  IntArray(int i) {}
+
+  ~IntArray() {}
+
   int add(int i);
   int get(size_t id) const;
   size_t size() const;
+  int last() const;
 };
 
-int main()
-{
+int main() {
   int next = 0;
   std::cin >> next;
 
-  IntArray a;
+  IntArray a(next);
 
-  a.add(next);
-
-  while (std::cin >> next)
-  {
+  while (std::cin >> next) {
     a.add(next);
   }
-  
 
-  if(!std::cin)
-  {
+  if (!std::cin) {
     return 1;
+  }
+
+  size_t count = 1;
+
+  for (size_t i = 0; i < a.size() - 1; ++i) {
+    int d = a.get(i);
+
+    count += !(d % a.last()) ? 1 : 0;
   };
 
-  for (size_t i = 0; i < a.size(); ++i)
-  {
-    int d = a.get(i);
-  }
-};
+  std::cout << count << "\n";
+}
