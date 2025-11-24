a8ec01a920c855845b3076ab9198159b689325d2
root <root@Asus.localdomain>
2025-11-24 23:39:17 +0300

    commit HW-1

--- main.cpp
+++ main.cpp
@@ -14,9 +14,30 @@ struct IntArray
         delete[] a;
     }
 
+    IntArray(const IntArray& rhs);
+    IntArray& operator=(const IntArray& rhs);
+
+    IntArray(IntArray&& rhs) :
+        a(rhs.a),
+        k(rhs.getsize())
+    {
+        rhs.a = nullptr;
+    }
+
+    IntArray& operator=(IntArray&& rhs)
+    {
+        delete[] a;
+        a = rhs.a;
+        k = rhs.getsize();
+
+        rhs.a = nullptr;
+        return *this;
+    }
+
     int add(int i);
     int get(size_t id) const noexcept;
-    size_t size() const noexcept;
+    /*int at(size_t id) const;*/
+    size_t getsize() const noexcept;
     int last() const noexcept;
 
     int* a;
@@ -44,9 +65,10 @@ int main()
 
         size_t count = 1;
 
-        for (size_t i = 0; i < a.size() - 1; ++i)
+        for (size_t i = 0; i < a.getsize() - 1; ++i)
         {
             int d = a.get(i);
+
             count += !(d % a.last()) ? 1 : 0;
         }
 
@@ -58,34 +80,59 @@ int main()
     }
 }
 
+IntArray::IntArray(const IntArray& rhs) :
+    a(new int[rhs.getsize()]),
+    k(getsize())
+{
+    for (size_t i = 0; i < rhs.getsize(); ++i)
+        a[i] = rhs.get(i);
+}
+
 int IntArray::get(size_t id) const
 {
     return a[id];
 }
 
-size_t IntArray::size() const noexcept
+//int IntArray::at(size_t id) const
+//{
+//
+//}
+
+size_t IntArray::getsize() const noexcept
 {
     return k;
 }
 
 int IntArray::last() const noexcept
 {
-    return get(size() - 1);
+    return get(getsize() - 1);
 }
 
 int IntArray::add(int i)
 {
-    int* dint = new int[size() + 1];
+    int* dint = new int[getsize() + 1];
 
-    for (size_t i = 0; i < size(); ++i)
+    for (size_t i = 0; i < getsize(); ++i)
     {
         dint[i] = get(i);
     }
 
     delete[] a;
     a = dint;
-    a[k] = i;
     ++k;
+}
 
-    return i;
+IntArray& IntArray::operator=(const IntArray& rhs)
+{
+    k = rhs.getsize();
+    int* p = new int[rhs.getsize()];
+
+    for (size_t i = 0; i < rhs.getsize(); ++i)
+        p[i] = rhs.get(i);
+
+    delete[] a;
+    a = p;
+    k = rhs.getsize();
+
+    return *this;
 }
